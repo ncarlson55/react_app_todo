981a65562bc4e669e0a8db51cfd45f78dd934ea3
Nico Carlson <nicocarlson285@gmail.com>
2021-04-27 23:25:38 -0500

    Material UI form control and input added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 
 function App() {
   // Setting up first state, our todos will start with an empty array
   const [todos, setTodos] = useState(['Take dogs for walk', 'Take trash out', 'wash dishes']); 
+  //State used to remember input
+  const [input, setInput] = useState('');
+  // Add Todo
+  const addToDo = (event) => {
+    console.log("Test Approved");
+    event.preventDefault(); //Prevent default refreshing of page 
+     //This will fire off when we click the button
+    setTodos([...todos, input]);
+    setInput(''); // clears the input after hitting enter
+    console.log(todos);
+  }
 
   return (
     <div className="App">
       <h1> Hello Nico! Here is your ToDo List:</h1>
-      <input/>
-      <button>Add ToDo</button>
 
+      <FormControl>
+        <InputLabel>Write a Todo</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)} />
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={addToDo} >Add ToDo</Button>
+      </FormControl>
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>
